15b7a6b38a9585d46e91d0ca2b3097a9ecefeffc
aravindagayan <1513223865@qq.com>
2020-01-04 20:39:18 +0800

    helper function to display images

---
+++ helpers.py
@@ -0,0 +1,24 @@
+import matplotlib as plt
+import matplotlib.pyplot as plt
+from numpy.random import randint
+
+def display_imgs(imgs, labels, class_names, fig_size):
+    """
+    Display Random Images
+
+    train_imgs: image list numpy array
+    train_labels: labels numpy array
+    class_names: list
+    num_images: int
+    fig_size: (width, height)
+    """
+    random_index = randint(0, len(imgs), 1)[0]
+    plt.figure(figsize=fig_size)
+    for i in range(25):
+        plt.subplot(5,5,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(True)
+        plt.imshow(imgs[random_index+i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[labels[random_index+i][0]])
+    plt.show()
